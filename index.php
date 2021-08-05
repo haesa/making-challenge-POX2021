67d7698b03968cbc76d0667722bb69ec99883fe2
haesa <pd6156@naver.com>
2021-08-05 23:53:06 +0900

    Delete code.php / Modify index.php
    exploit 코드에서 query를 get방식으로 전달 할 때 form도 함께 submit하는 방법을 몰라 code.php와 index.php를 합침.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" href="style.css">
@@ -6,17 +7,48 @@
     <body>
         <div class="wrap">
           <h2 class="tit">Blind SQL Injection</h2>
-          <form action="code.php" method="GET">
+          <form action="index1.php" method="GET">
               <input type="text" name="id" placeholder="ID">
               <input type="text" name="pw" placeholder="PW">
               <input type="submit" value="Login">
           </form>
           <div class="msg">Login to admin account.</div>
         </div>
+<?
+if(isset($_GET['id']) || isset($_GET['pw'])){
+    include './dbconn.php';
+
+    if(preg_match('/\'/i', $_GET['id'])) exit("~~filtering~~");
+    if(preg_match('/and|or|substr|ascii/i', $_GET['pw'])) exit("~~filtering~~");
+    $id = str_replace("admin","",$_GET['id']);
+    $pw = $_GET['pw'];
+
+    $query = "select id from user where id='{$id}';";
+    $result = mysqli_fetch_array(mysqli_query($conn, $query));
+
+    if(isset($result['id'])) {
+      $query = "select pw from user where id='{$id}' and pw='{$pw}';";
+      $result = mysqli_fetch_array(mysqli_query($conn, $query));
+
+      if(isset($result['pw'])) {
+        if($pw == $result['pw']) {
+          $msg = "Hello {$id} :)<br>";
+          if($id=='admin')  $msg = $msg."Problem solved!";
+        } else {
+          $msg = "You got the results!";
+        }
+      } else {
+        $msg = "You didn't get the results...";
+      }
+    } else {
+      $msg = "ID does not exist...";
+    }
+?>
+    <script type="text/javascript">
+      document.getElementsByClassName("msg")[0].innerHTML = "<? echo $msg; ?>"
+    </script>
+<?
+}
+?>
     </body>
 </html>
-<script type="text/javascript">
-  document.getElementsByName('id')[0].value = "<? echo $_GET['ID']; ?>";
-  document.getElementsByName('pw')[0].value = "<? echo $_GET['PW']; ?>";
-  document.getElementsByClassName("msg")[0].innerHTML = "<? echo $_GET['msg']; ?>";
-</script>
